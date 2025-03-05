749b673356dbd941fc65f41152c0817d20b1b3a0
shivm29 <developers@whozzat.com>
2025-03-05 11:51:15 +0530

    fixed gender input field

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -87,16 +87,18 @@ function Signup() {
         </div>
 
         <div className='flex w-full'>
-          <select
-            className='w-full rounded-lg p-3 mb-4 bg-input text-xs outline-none tracking-wider font-medium mr-3 text-xs'
-            value={gender} onChange={(e) => setGender(e.target.value)} required>
-            <option value="">SELECT GENDER</option>
-            {genderOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
+          <div className='w-full rounded-lg p-3 mb-4 bg-input text-xs outline-none tracking-wider font-medium mr-3 text-xs flex items-center' >
+            <select
+              className='w-full rounded-lg bg-input text-xs outline-none tracking-wider font-medium m text-xs'
+              value={gender} onChange={(e) => setGender(e.target.value)} required>
+              <option value="">SELECT GENDER</option>
+              {genderOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
 
           <input
             className='w-full rounded-lg p-3 mb-4 bg-input text-xs outline-none tracking-wider font-medium mr-3 text-sm placeholder:uppercase placeholder:text-white'
@@ -104,6 +106,7 @@ function Signup() {
             value={dob}
             onChange={(e) => setDob(e.target.value)}
             required
+            placeholder='DOB'
           />
         </div>
 
