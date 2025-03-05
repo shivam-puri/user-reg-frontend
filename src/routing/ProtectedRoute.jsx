4aa83dd6493a0481022ae930e3584ad8232a7d83
shivm29 <developers@whozzat.com>
2025-03-05 10:49:15 +0530

    fixed infinite loading

--- src/routing/ProtectedRoute.jsx
+++ src/routing/ProtectedRoute.jsx
@@ -36,7 +36,7 @@ const ProtectedRoute = () => {
         }
     }, [auth?.token, location]);
 
-    if (true) {
+    if (isLoading) {
         return <div className="flex items-center justify-center w-full h-screen" >
             <div className="loader" > </div>
         </div>;
