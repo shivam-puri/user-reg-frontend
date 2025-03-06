e95133dcd36133b62bb64c9c7a417be5dff5be13
shivm29 <developers@whozzat.com>
2025-03-06 15:42:07 +0530

    docs link added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 function Landing() {
 
     return (
-        <div className='flex w-full min-h-screen bg-secondary items-center justify-center' >
-            <Link to='/login' className='mt-6 flex items-center justify-center rounded-custom bg-white text-black p-3 w-40 text-xs font-semibold mr-5'>LOG IN</Link>
-            <Link to='/signup' className='mt-6 flex items-center justify-center rounded-custom bg-white text-black p-3 w-40 text-xs font-semibold '>SIGN UP</Link>
-
+        <div className='flex w-full flex-col min-h-screen bg-secondary items-center justify-center' >
+            <div className='flex w-full justify-center' >
+                <Link to='/login' className='mt-6 flex items-center justify-center rounded-custom bg-white text-black p-3 w-40 text-xs font-semibold mr-5'>LOG IN</Link>
+                <Link to='/signup' className='mt-6 flex items-center justify-center rounded-custom bg-white text-black p-3 w-40 text-xs font-semibold '>SIGN UP</Link>
+            </div>
+            <br />
+            <a href="https://github.com/shivam-puri/user-reg-backend/tree/main?tab=readme-ov-file#api-documentation" target="_blank" rel="noopener noreferrer">CLICK HERE TO GO TO THE REST API DOCUMENTATION</a>
         </div>
     )
 }
